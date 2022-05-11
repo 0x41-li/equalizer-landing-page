ec07a5c4c4409ea2d8e5618f0a0ac56a9b4728d7
Ali Sbane <ali.sbane.dev@gmail.com>
2022-05-11 23:51:46 +0100

    Mobile Version: Header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from "react";
 
+// images and logos
+import mainLogo from "../assets/logos/main-logo.png";
+
+// css
+import "../scss/header.scss";
+
 export default function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <div className="logo">
+        <img src={mainLogo} alt="" />
+      </div>
+    </header>
+  );
 }
